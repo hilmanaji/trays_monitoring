ff8c2426107c3d91ce21436e4b0983b80b01b6b6
HilmanAji <hilman17aji@gmail.com>
2026-05-28 11:37:14 +0700

    Add Windows runner support with DPI awareness and console output
    - Created a manifest file for the Windows runner to enable PerMonitorV2 DPI awareness.
    - Implemented utility functions for console creation and command line argument retrieval in utils.cpp and utils.h.
    - Developed the Win32Window class for managing a high DPI-aware window, including theme updates based on system preferences.
    - Added message handling for window events such as resizing and destruction.

---
+++ android/app/src/main/kotlin/com/example/trays_monitoring/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.trays_monitoring
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
---
+++ macos/Flutter/GeneratedPluginRegistrant.swift
@@ -0,0 +1,16 @@
+//
+//  Generated file. Do not edit.
+//
+
+import FlutterMacOS
+import Foundation
+
+import connectivity_plus
+import device_info_plus
+import flutter_secure_storage_macos
+
+func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
+  ConnectivityPlusPlugin.register(with: registry.registrar(forPlugin: "ConnectivityPlusPlugin"))
+  DeviceInfoPlusMacosPlugin.register(with: registry.registrar(forPlugin: "DeviceInfoPlusMacosPlugin"))
+  FlutterSecureStoragePlugin.register(with: registry.registrar(forPlugin: "FlutterSecureStoragePlugin"))
+}
